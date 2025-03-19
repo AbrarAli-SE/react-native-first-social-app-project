88c85dafa23002d81d794b24a6dc844d20b21603
AbrarAli-SE <abrarali.se@gmail.com>
2025-03-20 00:07:23 +0500

    schema and tables are created

--- socialApp/app/_layout.tsx
+++ socialApp/app/_layout.tsx
@@ -1,28 +1,18 @@
-import { Stack } from "expo-router";
-import { ClerkProvider } from '@clerk/clerk-expo'
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
-import { tokenCache } from "@/cache";
 import InitialLayout from "@/components/InitialLayout"; // Adjust the import path as necessary
-
-
-const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
-
-if (!publishableKey) {
-  throw new Error(
-    'Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env',
-  )
-}
+import ClerkAndConvexProvider from "@/providers/ClerkAndConvexProvider";
 
 export default function RootLayout() {
   return (
+    <ClerkAndConvexProvider>
 
-    <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
+
       <SafeAreaProvider>
         <SafeAreaView style={{ flex: 1, backgroundColor: "black" }}>
           <InitialLayout />
         </SafeAreaView>
       </SafeAreaProvider>
-    </ClerkProvider>
+    </ClerkAndConvexProvider>
   );
 
 }
